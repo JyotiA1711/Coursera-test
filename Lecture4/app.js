892450230e9d3502028b48cc8c6005fd55a6bf6f
Jyoti <jyoti.ambhore1711@gmail.com>
2021-06-03 18:29:55 -0500

    First AnjularJS App.

--- Lecture4/app.js
+++ Lecture4/app.js
@@ -3,7 +3,11 @@
 
 angular.module('myFirstApp', [])
 
-.controller('MyFirstController', function () {
+.controller('MyFirstController', function ($scope) {
+$scope.name = "Jyoti";
+$scope.sayHello = function(){
+  return "Thank you Coursera!!"
+}
 
 });
 
